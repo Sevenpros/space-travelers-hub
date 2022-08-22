f9d37fd673587ddef0d2b0334e362e0a03f6e79d
Sevenpros <sethbisoso@gmail.com>
2022-08-22 14:34:44 +0200

    Add header

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NavBar = () => (
+  <>
+    <header>
+      <nav className="nav-container">
+        <img src="../images/planet.png" alt="" className="image-logo" />
+        <h2>Space Traveler&#39;s Hub</h2>
+      </nav>
+    </header>
+  </>
+);
+
+export default NavBar;
