e70b8742ee0690db30d414cb13cfbdad83a17766
Sevenpros <sethbisoso@gmail.com>
2022-08-23 00:08:55 +0200

    Add rockets setup

---
+++ src/redux/rockets.js
@@ -0,0 +1,22 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+const api = 'https://api.spacexdata.com/v3/rockets';
+const FETCH_ROCKETS = 'FETCH_ROCKETS';
+const initialState = {
+  rockets: [],
+};
+
+const rocketRecuder = (state = initialState, action) => {
+  switch (action.type) {
+    case 'FETCH_ROCKETS':
+      return { ...state, rockets: action.payload };
+    default: return state;
+  }
+};
+
+export const fethRockets = createAsyncThunk(FETCH_ROCKETS, async () => {
+  const rockets = await (await fetch(api)).json();
+  return rockets;
+});
+
+export default rocketRecuder;
