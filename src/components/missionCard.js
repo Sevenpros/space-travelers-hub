74b94888a0cb2c4c547466838b4a4098299d5664
Abdullah2213565 <dulakhan024@gmail.com>
2022-08-24 11:36:13 +0200

    Up date my profile missions

--- src/components/missionCard.js
+++ src/components/missionCard.js
@@ -1,48 +1,49 @@
 import PropTypes from 'prop-types';
 import { useState } from 'react';
+import { joiningMissionAction, leavingMissionAction } from '../Redux/mission';
 
-const MissionCard = ({ description, name }) => {
-  const [reserved, setResereved] = useState(false);
+const MissionCard = ({ mission }) => {
+  const dispatch = useDispatch();
   return (
     <tr>
       <th style={{ minWidth: '170px' }}>
-        <p>{name}</p>
+        <p>{mission.name}</p>
       </th>
-      <td>{description}</td>
+      <td>{mission.description}</td>
       <td>
-        {reserved
+        {mission.reserved
           ? (
             <p
-              style={{ minWidth: '130px' }}
-              className="border bg-info text-light"
+              style={{ minWidth: '130px', marginTop: '40px' }}
+              className="border bg-info text-light text-center"
             >
               ACTIVE MEMBER
             </p>
           ) : (
             <p
-              style={{ minWidth: '130px' }}
-              className="border bg-secondary text-light"
+              style={{ minWidth: '130px', marginTop: '40px' }}
+              className="border bg-secondary text-light text-center"
             >
               NOT A MEMBER
             </p>
           )}
       </td>
       <td>
-        {!reserved
+        {!mission.reserved
           ? (
             <button
-              onClick={() => setResereved(true)}
+              onClick={() => dispatch(joiningMissionAction(mission.id))}
               type="button"
-              style={{ minWidth: '120px' }}
+              style={{ minWidth: '120px', marginTop: '40%' }}
               className="border border-success"
             >
               Join Mission
             </button>
           ) : (
             <button
-              onClick={() => setResereved(false)}
+              onClick={() => dispatch(leavingMissionAction(mission.id))}
               type="button"
-              style={{ minWidth: '120px' }}
+              style={{ minWidth: '120px', marginTop: '40%' }}
               className="border border-danger text-danger"
             >
               Leave Mission
@@ -52,10 +53,15 @@ const MissionCard = ({ description, name }) => {
     </tr>
   );
 };
-
 export default MissionCard;
 
 MissionCard.propTypes = {
   name: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
+  mission: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    reserved: PropTypes.bool,
+  }).isRequired,
 };
